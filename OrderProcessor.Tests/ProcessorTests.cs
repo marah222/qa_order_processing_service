2aa5a6109daeb660758348fdc002593e98c6f48f
marah222 <maraharam222@gmail.com>
2025-11-20 11:37:01 +0300

    feat: add ProcessorTests

---
+++ OrderProcessor.Tests/ProcessorTests.cs
@@ -0,0 +1,32 @@
+using Moq;
+
+namespace OrderProcessor.Tests
+{
+    public class ProcessorTests
+    {
+        private Mock<ICustomerRepository> _mockCustomerRepo;
+        private Processor _processor;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockCustomerRepo = new Mock<ICustomerRepository>();
+            _processor = new Processor(_mockCustomerRepo.Object);
+        }
+
+        [Test]
+        public void ProcessOrder_WithSufficientFunds_ApprovesOrder()
+        {
+            // Arrange
+            var customer = new Customer { AccountBalance = 200m };
+            _mockCustomerRepo.Setup(r => r.GetCustomer(1)).Returns(customer);
+            var order = new Order { Amount = 100m };
+
+            // Act
+            var result = _processor.ProcessOrder(1, order);
+
+            // Assert
+            Assert.IsTrue(result.IsApproved);
+        }
+    }
+}
