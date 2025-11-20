6eb9516ca71de50ff0ec3f2aeaceb26627cdcde1
marah222 <maraharam222@gmail.com>
2025-11-20 11:34:07 +0300

    feat: creat new test project and add base project as refrence

---
+++ OrderProcessor.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace OrderProcessor.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
